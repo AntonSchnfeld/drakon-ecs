59446613eeaa4b67133d497210d969b93001500e
schoenfelddev <schnfeldanton@gmail.com>
2025-09-04 09:40:18 +0200

    added `ComponentStore` to `EntityWorldImpl` to allow object component storage and added a usage example to `Main`
    - Updated `EntityWorld` interface to support component storage
    - Implemented simple Usage example in `Main`

--- src/main/java/io/github/antonschnfeld/drakon/ecs/EntityWorld.java
+++ src/main/java/io/github/antonschnfeld/drakon/ecs/EntityWorld.java
@@ -1,7 +1,19 @@
 package io.github.antonschnfeld.drakon.ecs;
 
+import java.util.List;
+
 public interface EntityWorld {
     long createEntity(Object... components);
+
+    List<Object> getComponents(long entity);
+
+    void addComponent(long entity, Object component);
+
+    <T> T getComponent(long entity, Class<T> componentType);
+
+    void removeComponent(long entity, Class<?> component);
+
+    void removeComponents(long entity);
 
     void destroyEntity(long entity);
 
--- src/main/java/io/github/antonschnfeld/drakon/ecs/EntityWorldImpl.java
+++ src/main/java/io/github/antonschnfeld/drakon/ecs/EntityWorldImpl.java
@@ -1,20 +1,56 @@
 package io.github.antonschnfeld.drakon.ecs;
 
+import java.util.List;
+
 public class EntityWorldImpl implements EntityWorld {
     private final EntityStore entityStore;
+    private final ComponentStore componentStore;
 
-    public EntityWorldImpl(EntityStore entityStore) {
-        this.entityStore = entityStore;
+    public EntityWorldImpl() {
+        this.entityStore = new EntityStore();
+        this.componentStore = new ComponentStore();
     }
 
     @Override
     public long createEntity(Object... components) {
-        return entityStore.createEntity();
+        long entity = entityStore.createEntity();
+
+        for (int i = 0; i < components.length; i++) {
+            componentStore.addComponent(entity, components[i]);
+        }
+
+        return entity;
+    }
+
+    @Override
+    public List<Object> getComponents(long entity) {
+        return componentStore.getComponents(entity);
+    }
+
+    @Override
+    public void addComponent(long entity, Object component) {
+        componentStore.addComponent(entity, component);
+    }
+
+    @Override
+    public <T> T getComponent(long entity, Class<T> componentType) {
+        return componentStore.getComponent(entity, componentType);
+    }
+
+    @Override
+    public void removeComponent(long entity, Class<?> component) {
+        componentStore.removeComponent(entity, component);
+    }
+
+    @Override
+    public void removeComponents(long entity) {
+        componentStore.removeComponents(entity);
     }
 
     @Override
     public void destroyEntity(long entity) {
         entityStore.deleteEntity(entity);
+        componentStore.removeComponents(entity);
     }
 
     @Override
